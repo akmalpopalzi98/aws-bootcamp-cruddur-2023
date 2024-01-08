8bd5a9cbd41dde74558bd3308e19f2c4c21e8159
Akmal Popalzi <akmalpopalzi.eng@gmail.com>
2024-01-08 13:13:27 +0000

    added instrumentation to notifications

--- backend-flask/services/notifications_activities.py
+++ backend-flask/services/notifications_activities.py
@@ -1,7 +1,12 @@
 from datetime import datetime, timedelta, timezone
+from aws_xray_sdk.core import xray_recorder
+
 class NotificationsActivities:
   def run():
+    segment = xray_recorder.begin_segment('notifications-segment')
     now = datetime.now(timezone.utc).astimezone()
+    segment.put_metadata('key', {now:now.isoformat()}, 'namespace')
+
     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
       'handle':  'coco',
@@ -23,4 +28,5 @@ class NotificationsActivities:
       }],
     }
     ]
+    xray_recorder.end_segment()
     return results
