abb49c4847f233fbae6cb64026e42ece9bed0c0a
Akmal Popalzi <akmalpopalzi.eng@gmail.com>
2024-01-10 17:32:19 +0000

    update amplify code - v6

--- frontend-react-js/src/pages/SigninPage.js
+++ frontend-react-js/src/pages/SigninPage.js
@@ -4,29 +4,37 @@ import {ReactComponent as Logo} from '../components/svg/logo.svg';
 import { Link } from "react-router-dom";
 
 // [TODO] Authenication
-import Cookies from 'js-cookie'
+
+import {signIn} from 'aws-amplify/auth'
 
 export default function SigninPage() {
 
-  const [email, setEmail] = React.useState('');
+  const [username, setUsername] = React.useState('');
   const [password, setPassword] = React.useState('');
   const [errors, setErrors] = React.useState('');
 
   const onsubmit = async (event) => {
-    event.preventDefault();
     setErrors('')
-    console.log('onsubmit')
-    if (Cookies.get('user.email') === email && Cookies.get('user.password') === password){
-      Cookies.set('user.logged_in', true)
-      window.location.href = "/"
-    } else {
-      setErrors("Email and password is incorrect or account doesn't exist")
+    event.preventDefault();
+    console.log(username)
+    try {
+      await signIn({username, password})
+        .then(user => {
+          localStorage.setItem("access_token", user.signInUserSession.accessToken.jwtToken)
+          window.location.href = "/"
+        })
+        .catch(err => { console.log('Error!', err) });
+    } catch (error) {
+      if (error.code == 'UserNotConfirmedException') {
+        window.location.href = "/confirm"
+      }
+      setErrors(error.message)
     }
     return false
   }
 
   const email_onchange = (event) => {
-    setEmail(event.target.value);
+    setUsername(event.target.value);
   }
   const password_onchange = (event) => {
     setPassword(event.target.value);
@@ -53,7 +61,7 @@ export default function SigninPage() {
               <label>Email</label>
               <input
                 type="text"
-                value={email}
+                value={username}
                 onChange={email_onchange} 
               />
             </div>
