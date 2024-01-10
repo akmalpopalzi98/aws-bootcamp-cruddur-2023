3f9f190938694f83e5bb610b7a665c60269ef26a
Akmal Popalzi <akmalpopalzi.eng@gmail.com>
2024-01-10 17:04:30 +0000

    fixing errors and updating code

--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -39,7 +39,7 @@ export default function HomeFeedPage() {
   };
 
   const checkAuth = async () => {
-    Auth.currentAuthenticatedUser({
+    await Auth.currentAuthenticatedUser({
       // Optional, By default is false. 
       // If set to true, this call will send a 
       // request to Cognito to get the latest user data
