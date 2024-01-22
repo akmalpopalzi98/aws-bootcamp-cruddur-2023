c33016dab3ea858f7314ee2ced9f93da6d031eb4
Akmal Popalzi <akmalpopalzi.eng@gmail.com>
2024-01-22 16:15:56 +0000

    updating backend

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,19 +1,32 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
 
-from lib.db import pool
-
+from lib.db import pool, query_wrap_array
 
 class HomeActivities:
   def run():
       now = datetime.now(timezone.utc).astimezone()
-      sql = """SELECT * FROM activities"""
+      sql = query_wrap_array("""
+      SELECT
+        activities.uuid,
+        users.display_name,
+        users.handle,
+        activities.message,
+        activities.replies_count,
+        activities.reposts_count,
+        activities.likes_count,
+        activities.reply_to_activity_uuid,
+        activities.expires_at,
+        activities.created_at
+      FROM public.activities
+      LEFT JOIN public.users ON users.uuid = activities.user_uuid
+      ORDER BY activities.created_at DESC
+      """)
+      print(sql)
       with pool.connection() as conn:
         with conn.cursor() as cur:
           cur.execute(sql)
           # this will return a tuple
           # the first field being the data
-          json = cur.fetchall()
-      print("-----")
-      print(json)
-      # return json[0]
+          json = cur.fetchone()
+      return json[0]
