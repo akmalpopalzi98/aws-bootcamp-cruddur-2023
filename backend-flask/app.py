98f43f4acf1516525fb4036d0f1e5f759d4744bb
Akmal Popalzi <akmalpopalzi.eng@gmail.com>
2024-01-25 16:13:39 +0000

    adding new files

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -40,11 +40,11 @@ from time import strftime
 
 
 # Initialize tracing and an exporter that can send data to Honeycomb
-provider = TracerProvider()
-processor = BatchSpanProcessor(OTLPSpanExporter())
-provider.add_span_processor(processor)
-trace.set_tracer_provider(provider)
-tracer = trace.get_tracer(__name__)
+# provider = TracerProvider()
+# processor = BatchSpanProcessor(OTLPSpanExporter())
+# provider.add_span_processor(processor)
+# trace.set_tracer_provider(provider)
+# tracer = trace.get_tracer(__name__)
 
 
 app = Flask(__name__)
